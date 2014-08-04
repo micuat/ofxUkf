547a3cc97d4c44507aa408495d0d7b729d007f43
micuat <micuat@aim.com>
2014-08-03 22:10:39 -0400

    Euler angle example (not using quaternion)

---
+++ example_euler/src/ofApp.cpp
@@ -0,0 +1,97 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ukf.init(0.001, 0.1);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	m.makeIdentityMatrix();
+	m.rotate(ofMap(mouseX, 0, ofGetWidth(), -180, 180), 1, 0, 0);
+	m.rotate(ofMap(mouseY, 0, ofGetHeight(), -180, 180), 0, 1, 0);
+	m.rotate(ofMap(ofGetElapsedTimef(), 0, 1, 0, 180), 0, 1, 1);
+	
+	ofQuaternion q;
+	q = m.getRotate();
+	
+	ofVec3f euler = q.getEuler();
+	for( int i = 0; i < 3; i++ ) {
+		float rev = floorf((eulerPrev[i] + 180) / 360.f) * 360;
+		euler[i] += rev;
+		if( euler[i] < -90 + rev && eulerPrev[i] > 90 + rev ) euler[i] += 360;
+		else if( euler[i] > 90 + rev && eulerPrev[i] < -90 + rev ) euler[i] -= 360;
+	}
+	
+	ukf.update(euler);
+	ofVec3f eulerPredicted = ukf.getEstimation();
+	
+	mPredicted.makeIdentityMatrix();
+	mPredicted.rotate(eulerPredicted.x, 1, 0, 0);
+	mPredicted.rotate(eulerPredicted.z, 0, 0, 1);
+	mPredicted.rotate(eulerPredicted.y, 0, 1, 0);
+	
+	eulerPrev = euler;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofBackground(54);
+	
+	cam.begin();
+	ofPushMatrix();
+	glMultMatrixf((GLfloat*)m.getPtr());
+	ofDrawAxis(100);
+	ofPopMatrix();
+	
+	ofPushMatrix();
+	glMultMatrixf((GLfloat*)mPredicted.getPtr());
+	ofDrawAxis(200);
+	ofPopMatrix();
+	cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
